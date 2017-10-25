a5d4d4cfc9fc442ecf104be0d28cb7766ec64589
DESKTOP-6SDFSSA\Ciprian <cipriannis@gmail.com>
2017-10-25 23:09:24 +0300

    Added custom Spliterator implementation

--- src/main/java/net/java8/part2/chapter7/WordCount.java
+++ src/main/java/net/java8/part2/chapter7/WordCount.java
@@ -1,6 +1,9 @@
 package net.java8.part2.chapter7;
 
+import java.util.Spliterator;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 /**
  * Created by ciprian on 10/25/17.
@@ -8,60 +11,114 @@ import java.util.stream.Stream;
 
 public class WordCount {
 
-  public static final String SENTENCE = " Nel   mezzo del cammin  di nostra  vita " +
-          "mi  ritrovai in una  selva oscura" +
-          " che la  dritta via era   smarrita ";
+    public static final String SENTENCE = " Nel   mezzo del cammin  di nostra  vita " +
+            "mi  ritrovai in una  selva oscura" +
+            " che la  dritta via era   smarrita ";
 
-  public static void main(String[] args) {
-    System.out.println("Word count iteratively: " + countWordsIteratively(SENTENCE));
-  }
-
-  private static int countWordsIteratively(String testString) {
-    int counter = 0;
-    boolean lastWasSpace = true;
-    for (char c : testString.toCharArray()) {
-      if (Character.isWhitespace(c)) {
-        lastWasSpace = true;
-      } else {
-        if (lastWasSpace) counter++;
-        lastWasSpace = false;
-      }
+    public static void main(String[] args) {
+        System.out.println("Word count iteratively: " + countWordsIteratively(SENTENCE));
+        System.out.println("Found " + countWords(SENTENCE) + " words");
     }
 
-    return counter;
-  }
+    private static int countWordsIteratively(String testString) {
+        int counter = 0;
+        boolean lastWasSpace = true;
+        for (char c : testString.toCharArray()) {
+            if (Character.isWhitespace(c)) {
+                lastWasSpace = true;
+            } else {
+                if (lastWasSpace) counter++;
+                lastWasSpace = false;
+            }
+        }
 
-  private static class WordCounter {
-    private final int counter;
-    private final boolean lastSpace;
-
-    public WordCounter(int counter, boolean lastSpace) {
-      this.counter = counter;
-      this.lastSpace = lastSpace;
+        return counter;
     }
 
-    public WordCounter accumulate(Character c) {
-      if(Character.isWhitespace(c)){
-        return lastSpace ? this : new WordCounter(counter, true);
-      }
-      else{
-        return lastSpace ? new WordCounter(counter + 1, false) : this;
-      }
+    private static class WordCounter {
+        private final int counter;
+        private final boolean lastSpace;
+
+        public WordCounter(int counter, boolean lastSpace) {
+            this.counter = counter;
+            this.lastSpace = lastSpace;
+        }
+
+        public WordCounter accumulate(Character c) {
+            if (Character.isWhitespace(c)) {
+                return lastSpace ? this : new WordCounter(counter, true);
+            } else {
+                return lastSpace ? new WordCounter(counter + 1, false) : this;
+            }
+        }
+
+        public WordCounter combine(WordCounter wordCounter) {
+            return new WordCounter(counter + wordCounter.counter, wordCounter.lastSpace);
+        }
+
+        public int getCounter() {
+            return counter;
+        }
     }
 
-    public WordCounter combine(WordCounter wordCounter){
-      return new WordCounter(counter + wordCounter.counter, wordCounter.lastSpace);
+    private static int countWords(Stream<Character> stream) {
+        WordCounter wordCounter = stream.reduce(new WordCounter(0, true),
+                WordCounter::accumulate,
+                WordCounter::combine);
+        return wordCounter.getCounter();
     }
 
-    public int getCounter() {
-      return counter;
-    }
-  }
+    public static int countWords(String s) {
+        Spliterator<Character> spliterator = new WordCounterSpliterator(s);
+        Stream<Character> stream = StreamSupport.stream(spliterator, true);
 
-  private static int countWords(Stream<Character> stream) {
-    WordCounter wordCounter = stream.reduce(new WordCounter(0, true),
-            WordCounter::accumulate,
-            WordCounter::combine);
-    return wordCounter.getCounter();
-  }
+        return countWords(stream);
+    }
+
+    private static class WordCounterSpliterator implements Spliterator<Character> {
+
+        private final String string;
+        private int currentChar = 0;
+
+        public WordCounterSpliterator(String string) {
+            this.string = string;
+            this.currentChar = currentChar;
+        }
+
+        @Override
+        public boolean tryAdvance(Consumer<? super Character> action) {
+            action.accept(string.charAt(currentChar++)); //Consume the current character
+            return currentChar < string.length(); //return true if there are further characters to be consumed
+        }
+
+        @Override
+        public Spliterator<Character> trySplit() {
+            int currentSize = string.length() - currentChar;
+            if (currentSize < 10) {
+                return null; //Return null to signal that the String to be parsed is small enough to be processed sequentially
+            }
+            for (int splitPos = currentSize / 2 + currentChar;
+                 splitPos < string.length(); splitPos++) {
+                if (Character.isWhitespace(string.charAt(splitPos))) {
+                    Spliterator<Character> spliterator =
+                            new WordCounterSpliterator(string.substring(currentChar,
+                                    splitPos));
+                    currentChar = splitPos;
+                    return spliterator;
+                }
+            }
+            return null;
+        }
+
+        @Override
+        public long estimateSize() {
+            return string.length() - currentChar;
+        }
+
+        @Override
+        public int characteristics() {
+            return ORDERED + SIZED + SUBSIZED + NONNULL + IMMUTABLE;
+        }
+    }
+
 }
